0d34f2e3966d58be52cfe5100f6a87b6b9e3aaac
saranyabhuvi <50897394+saranyabhuvi@users.noreply.github.com>
2019-05-27 12:23:05 +0530

    Update vowel1.py

--- vowel1.py
+++ vowel1.py
@@ -2,7 +2,7 @@ vol=input("")
 if(vol=='a' or vol=='e' or vol=='i' or vol=='o' or vol=='u'):
   print("Vowel")
 elif(vol=='b' or vol=='c' or vol=='d' or vol=='f' or vol=='g' or vol=='h' or vol=='j' or vol=='k' or vol=='l' or vol=='m' or vol=='n' or vol=='p' or vol=='q' or vol=='r' or vol=='s' or vol=='t' or vol=='v' or vol=='w' or vol=='x' or vol=='y' or vol=='z'):
-  print("consonant")
+  print("Consonant")
 else:
   print("invalid")
 
